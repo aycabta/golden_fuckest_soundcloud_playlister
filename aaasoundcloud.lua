1c44c8038a7a3af8884ab56c4c6a656164fa4dc6
Code Ass <aycabta@gmail.com>
2015-09-28 17:59:34 +0900

    Change indent to my Lua style

--- aaasoundcloud.lua
+++ aaasoundcloud.lua
@@ -14,46 +14,46 @@ function probe()
 end
 
 function parse()
-    line = vlc.readline()
-    if not line then return {} end
-    play_id = string.match(line, "\"soundcloud://playlists:(%d+)\"")
-    if not play_id then return {} end
-    local s, ejj = vlc.stream("http://api.soundcloud.com/playlists/" .. play_id .. ".json?client_id=" .. client_id)
-    if s == nil then return {} end
-    local buf = {}
-    line = nil
-    local new = s:readline()
-    while new ~= nil do
-      if line == nil then
-        line = new
-      else
-        line = line .. new
-      end
-      new = s:readline()
-    end
-    if not line then return {} end
-    json = require "dkjson"
-    json = json.decode(line)
-    buf = {}
-    for k,v in pairs(json.tracks) do
-      buf[#buf + 1 ] =
-          {
-            path = (v.stream_url .. "?client_id=" .. client_id),
-            name = v.title,
-            arturl = (v.artwork_url and v.artwork_url or v.user.artwork_url),
-            title = v.title,
-            artist = (v.user.username .. " (" .. v.user.permalink.. ")"),
-            genre = v.genre,
-            copyright = v.license,
-            description = v.description,
-            date = v.created_at,
-            url = vlc.access .. "://" .. v.permalink_url,
-            meta = 
-            {
-              ["tag list"] = v.tag_list,
-              ["creation time"] = v.created_at
-            }
-          }
-    end
-    return buf
+	line = vlc.readline()
+	if not line then return {} end
+	play_id = string.match(line, "\"soundcloud://playlists:(%d+)\"")
+	if not play_id then return {} end
+	local s, ejj = vlc.stream("http://api.soundcloud.com/playlists/" .. play_id .. ".json?client_id=" .. client_id)
+	if s == nil then return {} end
+	local buf = {}
+	line = nil
+	local new = s:readline()
+	while new ~= nil do
+		if line == nil then
+			line = new
+		else
+			line = line .. new
+		end
+		new = s:readline()
+	end
+	if not line then return {} end
+	json = require "dkjson"
+	json = json.decode(line)
+	buf = {}
+	for k,v in pairs(json.tracks) do
+		buf[#buf + 1 ] =
+		{
+			path = (v.stream_url .. "?client_id=" .. client_id),
+			name = v.title,
+			arturl = (v.artwork_url and v.artwork_url or v.user.artwork_url),
+			title = v.title,
+			artist = (v.user.username .. " (" .. v.user.permalink.. ")"),
+			genre = v.genre,
+			copyright = v.license,
+			description = v.description,
+			date = v.created_at,
+			url = vlc.access .. "://" .. v.permalink_url,
+			meta =
+			{
+				["tag list"] = v.tag_list,
+				["creation time"] = v.created_at
+			}
+		}
+	end
+	return buf
 end
