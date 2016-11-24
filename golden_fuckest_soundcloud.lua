c99a1b5275bee0743a9edbb2530e1e71d68945ac
Code Ass <aycabta@gmail.com>
2016-11-24 15:46:19 +0900

    Use local variable

--- golden_fuckest_soundcloud.lua
+++ golden_fuckest_soundcloud.lua
@@ -43,9 +43,9 @@ function parse()
 		chunk = s:read(4096)
 	end
 	if not json_data then return {} end
-	json = require "dkjson"
-	json = json.decode(line)
-	buf = {}
+	local json = require "dkjson"
+	json = json.decode(json_data)
+	local buf = {}
 	for k,v in pairs(json.tracks) do
 		buf[#buf + 1 ] =
 		{
